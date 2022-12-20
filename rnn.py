1bfc8bb20dd88e888230aed9ea097cb7d037072e
siafahim <git@fahimi.me>
2022-12-19 20:46:48 -0800

    building the MDN part

--- rnn.py
+++ rnn.py
@@ -1,4 +1,4 @@
-# Building the MDN-RNN model
+# Building the RNN model
 
 # Importing the libraries
 
@@ -26,4 +26,12 @@ class MDNRNN (object):
         self._init_session()
 
     # Making a method that creates the MDN-RNN model architecture itself
-    def _build_model(self, hps):
+    def build_model(self, hps):
+        # Building the RNN part of the MDN-RNN model
+        self.num_mixture = hps.num_mixture
+        KMIX = self.num_mixture
+        INWIDTH = hps.input_seq_width
+        OUTWIDTH = hps.output_seq_width
+        LENGTH = self.hps.max_seq_len
+        if hps.is_training:
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
