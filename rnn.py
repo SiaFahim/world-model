2ecfbfc88b9afdb3e5e8ec4c6c41fc89422a1fba
siafahim <git@fahimi.me>
2022-12-19 20:58:26 -0800

    creation of the LSTM cell and drropouts

--- rnn.py
+++ rnn.py
@@ -35,3 +35,18 @@ class MDNRNN (object):
         LENGTH = self.hps.max_seq_len
         if hps.is_training:
             self.global_step = tf.Variable(0, name='global_step', trainable=False)
+        cell_fn = tf.contrib.rnn.LayerNormBasicLSTMCell
+        use_recurrent_dropout = False if self.hps.use_recurrent_dropout == 0 else True
+        use_input_dropout = False if self.hps.use_input_dropout == 0 else True
+        use_output_dropout = False if self.hps.use_output_dropout == 0 else True
+        use_layer_norm = False if self.hps.use_layer_norm == 0 else True
+        if use_recurrent_dropout:
+            cell = cell_fn(hps.rnn_size, layer_norm=use_layer_norm, dropout_keep_prob=self.hps.recurrent_dropout_prob)
+        else:
+            cell = cell_fn(hps.rnn_size, layer_norm=use_layer_norm)
+        if use_input_dropout:
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, input_keep_prob=self.hps.input_dropout_prob)
+        if use_output_dropout:
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=self.hps.output_dropout_prob)
+        self.cell = cell
+        
